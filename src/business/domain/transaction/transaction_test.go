d584b636b244f016bacf577f777f4bcda1d4cf76
kobulwidodo <fadhilhan01@gmail.com>
2024-01-10 20:29:31 +0700

    test: coverage 100% with unit test

---
+++ src/business/domain/transaction/transaction_test.go
@@ -0,0 +1,91 @@
+package transaction
+
+import (
+	"database/sql"
+	"go-clean/src/business/entity"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Test_transaction_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	querySql := "INSERT INTO"
+	query := regexp.QuoteMeta(querySql)
+
+	mockTransaction := entity.Transaction{
+		UserID: 1,
+	}
+
+	type args struct {
+		transaction entity.Transaction
+	}
+	tests := []struct {
+		name        string
+		args        args
+		prepSqlMock func() (*sql.DB, error)
+		want        entity.Transaction
+		wantErr     bool
+	}{
+		{
+			name: "failed to create transaction",
+			args: args{
+				transaction: mockTransaction,
+			},
+			prepSqlMock: func() (*sql.DB, error) {
+				sqlServer, sqlMock, err := sqlmock.New()
+				sqlMock.ExpectQuery(query).WillReturnError(assert.AnError)
+				return sqlServer, err
+			},
+			want:    mockTransaction,
+			wantErr: true,
+		},
+		{
+			name: "all success",
+			args: args{
+				transaction: mockTransaction,
+			},
+			prepSqlMock: func() (*sql.DB, error) {
+				sqlServer, sqlMock, err := sqlmock.New()
+				sqlMock.ExpectBegin()
+				sqlMock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
+				sqlMock.ExpectCommit()
+				sqlMock.ExpectationsWereMet()
+				return sqlServer, err
+			},
+			want:    mockTransaction,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sqlServer, err := tt.prepSqlMock()
+			if err != nil {
+				t.Error(err)
+			}
+			defer sqlServer.Close()
+
+			sqlClient, err := gorm.Open(mysql.New(mysql.Config{
+				Conn:                      sqlServer,
+				SkipInitializeWithVersion: true,
+			}))
+			if err != nil {
+				t.Error(err)
+			}
+
+			u := Init(sqlClient)
+			_, err = u.Create(tt.args.transaction)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("transaction.Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
---
+++ src/business/usecase/midtrans_transaction/midtrans_transaction_test.go
@@ -0,0 +1,264 @@
+package midtranstransaction_test
+
+import (
+	mock_cart "go-clean/src/business/domain/mock/cart"
+	mock_midtrans "go-clean/src/business/domain/mock/midtrans"
+	mock_midtranstransaction "go-clean/src/business/domain/mock/midtrans_transaction"
+	"go-clean/src/business/entity"
+	"testing"
+
+	midtranstransaction "go-clean/src/business/usecase/midtrans_transaction"
+
+	"github.com/midtrans/midtrans-go/coreapi"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"gorm.io/gorm"
+)
+
+func Test_midtransTransaction_HandleNotification(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	midtransMock := mock_midtrans.NewMockInterface(ctrl)
+	midtransTransactionMock := mock_midtranstransaction.NewMockInterface(ctrl)
+	cartMock := mock_cart.NewMockInterface(ctrl)
+
+	payloadMock := map[string]interface{}{
+		"order_id": "1",
+	}
+
+	transactionResponseMock := &coreapi.TransactionStatusResponse{
+		TransactionStatus: "capture",
+		FraudStatus:       "accept",
+	}
+
+	transactionResponseChallengeMock := &coreapi.TransactionStatusResponse{
+		TransactionStatus: "capture",
+		FraudStatus:       "challenge",
+	}
+
+	transactionResponseSettlementMock := &coreapi.TransactionStatusResponse{
+		TransactionStatus: "settlement",
+	}
+
+	transactionResponseDenyMock := &coreapi.TransactionStatusResponse{
+		TransactionStatus: "deny",
+	}
+
+	transactionResponseCancelMock := &coreapi.TransactionStatusResponse{
+		TransactionStatus: "cancel",
+	}
+
+	transactionResponsePendingMock := &coreapi.TransactionStatusResponse{
+		TransactionStatus: "pending",
+	}
+
+	midtransTransactionParamMock := entity.MidtransTransactionParam{
+		OrderID: "1",
+	}
+
+	midtransTransactionResultMock := entity.MidtransTransaction{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		TransactionID: 1,
+	}
+
+	midtransTransactionUpdateParamMock := entity.MidtransTransactionParam{
+		ID: 1,
+	}
+
+	midtransTransactionUpdateMock := entity.UpdateMidtransTransactionParam{
+		Status: entity.StatusSuccess,
+	}
+
+	midtransTransactionUpdateChallangeMock := entity.UpdateMidtransTransactionParam{
+		Status: entity.StatusChallange,
+	}
+
+	midtransTransactionUpdateDenyMock := entity.UpdateMidtransTransactionParam{
+		Status: entity.StatusDeny,
+	}
+
+	midtransTransactionUpdateFailureMock := entity.UpdateMidtransTransactionParam{
+		Status: entity.StatusFailure,
+	}
+
+	midtransTransactionUpdatePendingMock := entity.UpdateMidtransTransactionParam{
+		Status: entity.StatusPending,
+	}
+
+	cartUpdateParamMock := entity.CartParam{
+		Status:        entity.StatusUnpaid,
+		TransactionID: 1,
+	}
+
+	cartUpdateMock := entity.UpdateCartParam{
+		Status: entity.StatusPaid,
+	}
+
+	mt := midtranstransaction.Init(midtransTransactionMock, midtransMock, cartMock)
+
+	type mockFields struct {
+		midtrans             *mock_midtrans.MockInterface
+		midtrans_transaction *mock_midtranstransaction.MockInterface
+		cart                 *mock_cart.MockInterface
+	}
+
+	mocks := mockFields{
+		midtrans:             midtransMock,
+		midtrans_transaction: midtransTransactionMock,
+		cart:                 cartMock,
+	}
+
+	type args struct {
+		payload map[string]interface{}
+	}
+
+	tests := []struct {
+		name     string
+		args     args
+		mockFunc func(mock mockFields, arg args)
+		wantErr  bool
+	}{
+		{
+			name: "failed to get order id",
+			args: args{
+				payload: map[string]interface{}{},
+			},
+			mockFunc: func(mock mockFields, arg args) {
+			},
+			wantErr: true,
+		},
+		{
+			name: "failed to handle midtrans",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(nil, assert.AnError)
+			},
+			wantErr: true,
+		},
+		{
+			name: "failed get midtrans transaction",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(entity.MidtransTransaction{}, assert.AnError)
+			},
+			wantErr: true,
+		},
+		{
+			name: "failed get update midtrans transaction",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdateMock).Return(assert.AnError)
+			},
+			wantErr: true,
+		},
+		{
+			name: "failed update cart",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdateMock).Return(nil)
+				mock.cart.EXPECT().Update(cartUpdateParamMock, cartUpdateMock).Return(assert.AnError)
+			},
+			wantErr: true,
+		},
+		{
+			name: "all success",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdateMock).Return(nil)
+				mock.cart.EXPECT().Update(cartUpdateParamMock, cartUpdateMock).Return(nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "all success settlement",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseSettlementMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdateMock).Return(nil)
+				mock.cart.EXPECT().Update(cartUpdateParamMock, cartUpdateMock).Return(nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "all success challenge",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseChallengeMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdateChallangeMock).Return(nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "all success deny",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseDenyMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdateDenyMock).Return(nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "all success cancel",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponseCancelMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdateFailureMock).Return(nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "all success pending",
+			args: args{
+				payload: payloadMock,
+			},
+			mockFunc: func(mock mockFields, arg args) {
+				mock.midtrans.EXPECT().HandleNotification("1").Return(transactionResponsePendingMock, nil)
+				mock.midtrans_transaction.EXPECT().Get(midtransTransactionParamMock).Return(midtransTransactionResultMock, nil)
+				mock.midtrans_transaction.EXPECT().Update(midtransTransactionUpdateParamMock, midtransTransactionUpdatePendingMock).Return(nil)
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mockFunc(mocks, tt.args)
+			err := mt.HandleNotification(tt.args.payload)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("midtransTransaction.HandleNotification() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
