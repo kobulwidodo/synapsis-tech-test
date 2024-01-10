2e077071e23f6fc7280d6cefbc8504d8d46acc35
kobulwidodo <fadhilhan01@gmail.com>
2024-01-10 21:15:19 +0700

    feat: create new endpoint for get payment detail

--- src/handler/rest/midtrans_transaction.go
+++ src/handler/rest/midtrans_transaction.go
@@ -2,10 +2,39 @@ package rest
 
 import (
 	"encoding/json"
+	"go-clean/src/business/entity"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+// @Summary Get Payment Detail
+// @Description Get Payment Detail by Transaction ID
+// @Security BearerAuth
+// @Tags Midtrans Transaction
+// @Produce json
+// @Param transaction_id path integer true "transaction id"
+// @Success 200 {object} entity.Response{data=entity.MidtransTransactionPaymentDetail{}}
+// @Failure 400 {object} entity.Response{}
+// @Failure 401 {object} entity.Response{}
+// @Failure 404 {object} entity.Response{}
+// @Failure 500 {object} entity.Response{}
+// @Router /api/v1/transaction/{transaction_id}/payment-detail [GET]
+func (r *rest) GetPaymentDetail(ctx *gin.Context) {
+	var param entity.MidtransTransactionParam
+	if err := ctx.ShouldBindUri(&param); err != nil {
+		r.httpRespError(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	result, err := r.uc.MidtransTransaction.GetPaymentDetail(param)
+	if err != nil {
+		r.httpRespError(ctx, http.StatusInternalServerError, err)
+		return
+	}
+
+	r.httpRespSuccess(ctx, http.StatusOK, "successfully get payment detail", result)
+}
 
 func (r *rest) HandleNotification(ctx *gin.Context) {
 	var notifPayload map[string]interface{}
