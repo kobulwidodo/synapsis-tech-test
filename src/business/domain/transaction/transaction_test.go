f028f880f9234397d5b88156c2c38a5bfda33b33
kobulwidodo <fadhilhan01@gmail.com>
2024-01-10 21:14:34 +0700

    test: coverage 100% with unit test

--- src/business/domain/transaction/transaction_test.go
+++ src/business/domain/transaction/transaction_test.go
@@ -89,3 +89,80 @@ func Test_transaction_Create(t *testing.T) {
 		})
 	}
 }
+
+func Test_transaction_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	querySql := "SELECT * FROM `transactions` WHERE `transactions`.`deleted_at` IS NULL"
+	query := regexp.QuoteMeta(querySql)
+
+	mockParam := entity.TransactionParam{}
+
+	type args struct {
+		param entity.TransactionParam
+	}
+	tests := []struct {
+		name        string
+		args        args
+		prepSqlMock func() (*sql.DB, error)
+		want        entity.Transaction
+		wantErr     bool
+	}{
+		{
+			name: "failed to exec query",
+			args: args{
+				param: mockParam,
+			},
+			prepSqlMock: func() (*sql.DB, error) {
+				sqlServer, sqlMock, err := sqlmock.New()
+				sqlMock.ExpectQuery(query).WillReturnError(assert.AnError)
+				return sqlServer, err
+			},
+			want:    entity.Transaction{},
+			wantErr: true,
+		},
+		{
+			name: "all ok",
+			args: args{
+				param: mockParam,
+			},
+			prepSqlMock: func() (*sql.DB, error) {
+				sqlServer, sqlMock, err := sqlmock.New()
+				row := sqlmock.NewRows([]string{"user_id"})
+				row.AddRow("1")
+				sqlMock.ExpectQuery(query).WillReturnRows(row)
+				return sqlServer, err
+			},
+			want: entity.Transaction{
+				UserID: 1,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sqlServer, err := tt.prepSqlMock()
+			if err != nil {
+				t.Error(err)
+			}
+			defer sqlServer.Close()
+
+			sqlClient, err := gorm.Open(mysql.New(mysql.Config{
+				Conn:                      sqlServer,
+				SkipInitializeWithVersion: true,
+			}))
+			if err != nil {
+				t.Error(err)
+			}
+
+			u := Init(sqlClient)
+			got, err := u.Get(tt.args.param)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("transaction.Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
